82037147b434876ebab03679585d73f3541ced9f
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-02-19 18:56:48 +0100

    Debug script (currently showing rabbits target)

---
+++ scripts/dbg_draw.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+@export var camera: Camera3D
+@onready var simulation := get_parent() as Simulation
+
+
+func draw_rabbits_targets() -> void:
+	print("draw_rabbits_targets")
+	for rabbit in simulation.rabbits:
+		var rabbit_target := rabbit.target_tile
+		if rabbit_target == null:
+			continue
+		var rabbit_screen_pos := camera.unproject_position(rabbit.position)
+		var rabbit_target_screen_pos := camera.unproject_position(rabbit_target.position)
+		draw_line(rabbit_screen_pos, rabbit_target_screen_pos, Color.GREEN, 1.0)
+
+
+func _draw() -> void:
+	draw_rabbits_targets()
+
+
+func _process(delta: float) -> void:
+	queue_redraw()
